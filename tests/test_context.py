65665075349febc6c9c9025a1242a2c560a7e46f
Maciej Mikulski <maciej.mikulski.jr@gmail.com>
2018-08-21 00:14:18 +0200

    Add unittests fot interval property

--- tests/test_context.py
+++ tests/test_context.py
@@ -16,9 +16,23 @@ class TimedContextTest(unittest.TestCase):
 
         self.assertTrue(print_str.startswith('0.1'))
 
+    def test_interval_property(self):
+        with Timing(print_fn=None, unit='ms') as t:
+            # should be zero at beginning
+            self.assertAlmostEqual(t.interval, 0, delta=0.02)
 
+            # check in meantime
+            sleep(0.15)
+            self.assertAlmostEqual(t.interval, 0.15, delta=0.02)
 
+            sleep(0.15)
 
+        # check after the context was exited
+        self.assertAlmostEqual(t.interval, 0.3, delta=0.02)
+
+        # make sure timing was stopped after the context was exited
+        sleep(0.15)
+        self.assertAlmostEqual(t.interval, 0.3, delta=0.02)
 
 if __name__ == '__main__':
     unittest.main()
