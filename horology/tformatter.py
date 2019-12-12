c9a6393133668bddb56272a3b462332e9dce166f
John Ashurst - MAFFAS <johnashu1@hotmail.com>
2019-12-13 00:24:36 +0100

    [UPD} Dict to tuples, rename for verbosity and add doc string

--- horology/tformatter.py
+++ horology/tformatter.py
@@ -1,37 +1,53 @@
 
-UNITS = {
-    'ns': 10 ** -9,
-    'us': 10 ** -6,
-    'ms': 10 ** -3,
-    's': 1,
-    'min': 60,
-    'h': 3600,
-    'd': 3600 * 24
-}  # todo: allow alternatives, eg. h == hs == hours
+UNITS = (
+    (('ns'), (10 ** -9)),
+    (('us'), (10 ** -6)),
+    (('ms'), (10 ** -3)),
+    (('s'), (1)),
+    (('min'), (60)),
+    (('h', 'hs', 'hours'), (3600)),
+    (('d'), (3600 * 24))
+  )
 
 
-def rescale_time(t, unit):
+def rescale_time(interval, unit):
+    """
+        Calculate and return a readable tuple from the arguments passed.
+        
+        Arguments:
+            interval {float} -- Interval
+            unit {str} -- String of Unit type.
+        
+        Raises:
+            ValueError: Unknown unit: {unit}. Use one of the following: {[x[0] for x in UNITS]} or 'auto' 
+        
+        Returns:
+            tuple -- interval / calculation, unit
+    """
     unit = unit.lower()
+
     if unit in ('auto', 'a'):
-        unit = auto_unit(t)
-    if unit not in UNITS.keys():
-        raise ValueError(f"Unknown unit: {unit}. Use one od those: {UNITS.keys()} or 'auto'")
+        unit = auto_unit(interval)
 
-    return t / UNITS[unit], unit
+    for units, calculation  in UNITS:
+        if unit in units:
+            return interval / calculation, unit
+            
+    raise ValueError(f"Unknown unit: {unit}. Use one of the following: {[x[0] for x in UNITS]} or 'auto'")
+    
 
-
-def auto_unit(t):
-    if t < 10 ** -6:
+def auto_unit(interval):
+    if interval < 10 ** -6:
         return 'ns'
-    if t < 10 ** -3:
+    elif interval < 10 ** -3:
         return 'us'
-    if t < 1:
+    elif interval < 1:
         return 'ms'
-    if t < 10 ** 3:
+    elif interval < 10 ** 3:
         return 's'
-    if t < 6 * 10 ** 4:
+    elif interval < 6 * 10 ** 4:
         return 'min'
-    if t < 36 * 10 ** 5:
+    elif interval < 36 * 10 ** 5:
         return 'h'
-
-    return 'd'
+    else:
+        return 'd'
