b173e86e4daa176631084ed25a333fed28ffa053
John Ashurst - MAFFAS <johnashu1@hotmail.com>
2019-12-17 10:18:01 +0100

    [ADD] namedtuple for UNITS. left indent comments

--- horology/tformatter.py
+++ horology/tformatter.py
@@ -1,46 +1,55 @@
 
-# A tuple of tuples containing:
-# ( <unit_names>, <rescale_calc>, <auto_cal> )
+def return_units():
+    """
+    Create a namedtuple of data.  Can be unpacked or accessed using dot notation.
+    
+    Returns:
+        namedtuple -- names scale unit
+    """
+    from collections import namedtuple
 
-UNITS = (
-    (('ns',), 10 ** -9, 10 ** -6),
-    (('us',), 10 ** -6, 10 ** -3),
-    (('ms',), 10 ** -3, 1),
-    (('s',), 1,  10 ** 3),
-    (('min',), 60, 6 * 10 ** 4),
-    (('h', 'hs', 'hours'), 3600, 36 * 10 ** 5),
-    (('d',), 3600 * 24, None)
-  )
+    Unit = namedtuple('Unit', 'names scale limit')
+
+    return (
+        Unit(('ns',), 10 ** -9, 10 ** -6),
+        Unit(('us',), 10 ** -6, 10 ** -3),
+        Unit(('ms',), 10 ** -3, 1),
+        Unit(('s',), 1,  10 ** 3),
+        Unit(('min',), 60, 6 * 10 ** 4),
+        Unit(('h', 'hs', 'hours'), 3600, 36 * 10 ** 5),
+        Unit(('d',), 3600 * 24, None)
+    )
 
 
 def rescale_time(interval, unit):
     """
-        Calculate and return a readable tuple from the arguments passed.
-        
-        Arguments:
-            interval {float} -- Interval
-            unit {str} -- String of Unit type.
-        
-        Raises:
-            ValueError: Unknown unit: {unit}. Use one of the following: {[x[0] for x in UNITS]} or 'auto' 
-        
-        Returns:
-            tuple -- interval / calculation, unit
+    Calculate and return a readable tuple from the arguments passed.
+    
+    Arguments:
+        interval {float} -- Interval
+        unit {str} -- String of Unit type.
+    
+    Raises:
+        ValueError: Unknown unit: {unit}. Use one of the following: {[x[0] for x in UNITS]} or 'auto' 
+    
+    Returns:
+        tuple -- interval / calculation, unit
     """
     unit = unit.lower()
 
     if unit in ('auto', 'a'):
         unit = auto_unit(interval)
 
-    for units, calculation, _  in UNITS:
+    for units, calculation, _  in return_units():
         if unit in units:
             return interval / calculation, unit
             
-    raise ValueError(f"Unknown unit: {unit}. Use one of the following: {[x[0] for x in UNITS]} or 'auto'")
+    raise ValueError(f"Unknown unit: {unit}. Use one of the following: {[x[0] for x in return_units()]} or 'auto'")
     
 
 def auto_unit(interval):
-    """Automatically find a unit from the interval passed. 
+    """
+    Automatically find a unit from the interval passed. 
     
     Arguments:
         interval {float} -- Interval
@@ -50,7 +59,7 @@ def auto_unit(interval):
                 Will return the 1st item in the tuple or 'd'.
     """
 
-    for units, _, find_unit_sum in UNITS:
+    for units, _, find_unit_sum in return_units():
         if interval < find_unit_sum:
             return units[0]
     return 'd'
