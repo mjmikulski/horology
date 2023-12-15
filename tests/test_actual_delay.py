34f450a237c549ec27df8f276193af337703fff3
mjmikulski <maciej.mikulski.jr@gmail.com>
2023-12-15 23:14:27 +0100

    Rerun failed sleep tests

--- tests/test_actual_delay.py
+++ tests/test_actual_delay.py
@@ -2,14 +2,16 @@ from contextlib import redirect_stdout
 from io import StringIO
 from time import sleep
 
+import pytest
+
 from horology import Timed, Timing, timed
 
-
+@pytest.mark.flaky(reruns=5)
 class TestWithSleep:
     def test_context(self):
         with redirect_stdout(out := StringIO()):
             with Timing():
-                sleep(0.15)
+                sleep(0.11)
             print_str = out.getvalue().strip()
 
         assert print_str.startswith('1')
@@ -18,7 +20,7 @@ class TestWithSleep:
     def test_decorator(self):
         @timed
         def foo():
-            sleep(0.15)
+            sleep(0.11)
 
         with redirect_stdout(out := StringIO()):
             foo()
@@ -30,7 +32,7 @@ class TestWithSleep:
     def test_iterable(self):
         with redirect_stdout(out := StringIO()):
             for _ in Timed(range(1)):
-                sleep(0.15)
+                sleep(0.11)
             print_str = out.getvalue().strip()
 
         assert print_str.startswith('iteration    1: 1')
